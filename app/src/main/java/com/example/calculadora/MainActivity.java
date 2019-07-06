cba07abb3020f536c8423b3dac76381287877b13
juliopernett <jjpernett2008@gmail.com>
2019-07-06 16:13:29 -0500

    calculadora básica terminada

--- app/src/main/java/com/example/calculadora/MainActivity.java
+++ app/src/main/java/com/example/calculadora/MainActivity.java
@@ -11,7 +11,7 @@ import android.widget.TextView;
 public class MainActivity extends AppCompatActivity {
     TextView resultado;
     EditText num1, num2;
-    double n1,n2, suma;
+    double n1, n2;
     String opciones[];
     Spinner combo;
 
@@ -38,12 +38,64 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    public void calcular(View v){
-        n1 = Double.parseDouble(num1.getText().toString());
-        n2 = Double.parseDouble(num2.getText().toString());
-        suma = n1 + n2;
-        resultado.setText(""+suma);
+    public void calcular(View v) {
+        double res = 0;
+        int opc;
+        if (validar()) {
+            n1 = Double.parseDouble(num1.getText().toString());
+            n2 = Double.parseDouble(num2.getText().toString());
+            opc = combo.getSelectedItemPosition();
 
+            switch (opc) {
+                case 0:
+                    res = n1 + n2;
+                    break;
+                case 1:
+                    res = n1 - n2;
+                    break;
+                case 2:
+                    res = n1 * n2;
+                    break;
+                case 3:
+                    res = n1 / n2;
+
+                    break;
+            }
+            //imrpimir consola
+            //log.d("prueba", "valor del resultado: " + res);")
+            resultado.setText("" + res);
+        }
+    }
+
+    public void limpiar(View v) {
+        num1.setText("");
+        num2.setText((""));
+        resultado.setText("");
+        combo.setSelection(0);
+        num1.requestFocus();
+    }
+
+    public boolean validar() {
+        int opc = combo.getSelectedItemPosition();
+        if (num1.getText().toString().isEmpty()) {
+            /*num1.setError("Digite el número uno");*/
+            num1.setError(getResources().getString(R.string.error_1));
+            num1.requestFocus();
+            return false;
+        } else if (num2.getText().toString().isEmpty()) {
+            /*num2.setError("Digite el número uno");*/
+            num2.setError(getResources().getString(R.string.error_2));
+            num2.requestFocus();
+            return false;
+        } else if (Double.parseDouble(num2.getText().toString()) == 0
+                && (opc == 3)) {
+            /*num2.setError("el segundo número no puede ser cero");*/
+            num2.setError(getResources().getString(R.string.error_3));
+            num2.selectAll();
+            return false;
+        }
+        return true;
     }
 
 }
+
